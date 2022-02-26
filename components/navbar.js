9feaa4612c6b54485a024df8da8b790cae249f45
mtmusic <michael-r-turner@outlook.com>
2022-02-26 14:38:18 -0800

    some minor updates

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,7 @@
 class NavBar extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <div class="flex m-0 p-4 items-center justify-between bg-gradient-to-r bg-zinc-700 text-white">
+        <div class="flex m-0 p-4 items-center justify-between bg-gradient-to-r bg-zinc-800 text-white">
             <h1 class="text-3xl">Thumbtack Web</h1>
             <div>
                 <js-navbaritem text="About" to="/about.html"></js-navbaritem>
